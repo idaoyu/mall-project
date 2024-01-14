7b18ee069134aee683236a1eb6a29a29157e1c8a
chenzx <qchenzexuan@vip.qq.com>
2024-01-14 14:32:03 +0800

    feat: 添加所需依赖项、添加项目配置

--- src/main/java/com/chenzx/project/MallProjectApplication.java
+++ src/main/java/com/chenzx/project/MallProjectApplication.java
@@ -3,6 +3,10 @@ package com.chenzx.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 一条秋刀鱼zz qchenzexuan@vip.qq.com
+ * @since 2024/1/14 14:15
+ **/
 @SpringBootApplication
 public class MallProjectApplication {
 
